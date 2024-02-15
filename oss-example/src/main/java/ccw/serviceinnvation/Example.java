820d9d8363ea442f6b9bfd586700335c46b7dee5
cx <3207242935@qq.com>
2024-02-15 19:18:36 +0800

    fix: readme.md

---
+++ oss-example/src/main/java/ccw/serviceinnvation/Example.java
@@ -0,0 +1,28 @@
+package ccw.serviceinnvation;
+
+import ccw.serviceinnovation.hash.checksum.EtagHandler;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.Checksum;
+
+public class Example {
+    public static void main(String[] args) throws IOException {
+        OssRestfulClient ossRestfulClient = new OssRestfulClient("localhost", 8080, "root", "123456");
+        EtagHandler etagHandler = ossRestfulClient.etagHandler;
+        ossRestfulClient.login();
+        String bucketName = "test";
+        String objectName = "2.jpg";
+        Path path = Paths.get(".\\"+objectName);
+        ossRestfulClient.createBucket(bucketName);
+        ossRestfulClient.upload(bucketName,path.toString());
+        Checksum deserialize = etagHandler.deserialize("0");
+        byte[] bytes = Files.readAllBytes(path);
+        deserialize.update(bytes,0,bytes.length);
+        System.out.println(deserialize.getValue());
+        ossRestfulClient.download(bucketName,objectName, "D:\\oss\\test\\");
+
+    }
+}
